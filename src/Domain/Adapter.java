740aa46cabdb146752e3310103df425762a2f65a
linerebo <55693839+linerebo@users.noreply.github.com>
2020-05-21 13:19:46 +0200

    added cleaningCentralSingleton to Adaptor Class

--- src/Domain/Adapter.java
+++ src/Domain/Adapter.java
@@ -1,4 +1,15 @@
 package Domain;
 
+import Domain.CleaningCentral;
+
 public class Adapter {
+    private static CleaningCentral cleaningCentralSingleton;
+
+    //returns the instance. If there is no instance, it makes a new one. Using the Singleton pattern.
+    public static CleaningCentral cleaningCentralInstance(){
+        if(cleaningCentralSingleton == null){
+            cleaningCentralSingleton = new CleaningCentral();
+        }
+        return cleaningCentralSingleton;
+    }
 }
