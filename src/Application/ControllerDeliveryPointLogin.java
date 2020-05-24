bc0354cbe8883850896599691b9786dc3ff0d79f
linerebo <55693839+linerebo@users.noreply.github.com>
2020-05-24 20:32:16 +0200

    added menuButton to login fxml windows

--- src/Application/ControllerDeliveryPointLogin.java
+++ src/Application/ControllerDeliveryPointLogin.java
@@ -5,16 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.fxml.*;
 
 import java.io.IOException;
 
 public class ControllerDeliveryPointLogin {
 
+    @FXML
+    MenuButton menuButton1;
+    @FXML
+    MenuItem menuItem3;
+
     public void handleButtonHome(ActionEvent event) throws IOException {
         Parent menuScreen = FXMLLoader.load(getClass().getResource("/Presentation/welcome.fxml"));
         Scene Scene = new Scene(menuScreen);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) menuButton1.getScene().getWindow();
         window.setScene(Scene);
         window.show();
     }
--- src/Application/ControllerDriverLogin.java
+++ src/Application/ControllerDriverLogin.java
@@ -1,20 +1,28 @@
 package Application;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ControllerDriverLogin {
 
+    @FXML
+    MenuButton menuButton2;
+    @FXML
+    MenuItem menuItem3;
+
     public void handleButtonHome(ActionEvent event) throws IOException {
         Parent menuScreen = FXMLLoader.load(getClass().getResource("/Presentation/welcome.fxml"));
         Scene Scene = new Scene(menuScreen);
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) menuButton2.getScene().getWindow();
         window.setScene(Scene);
         window.show();
     }
