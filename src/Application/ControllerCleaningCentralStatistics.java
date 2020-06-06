a04dc1347c76fc3b5e63672ad1382226960de42f
Jette <jettechristiansen09@gmail.com>
2020-06-06 20:53:47 +0200

    1) modifications ControllerCleaningCentralStatistics now showing chart for incoming orders 2) added new stored procedure for the statistics data -incoming orders (script on trello) 3) added new method to DB for statistics

--- src/Application/ControllerCleaningCentralStatistics.java
+++ src/Application/ControllerCleaningCentralStatistics.java
@@ -2,23 +2,17 @@ package Application;
 
 import Domain.Adapter;
 import Domain.SystemUser.SystemUser;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.*;
-import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ControllerCleaningCentralStatistics {
 
@@ -26,10 +20,7 @@ public class ControllerCleaningCentralStatistics {
     @FXML MenuButton menuButton1;
     @FXML MenuItem menuItem3;
     @FXML PieChart pieChart;
-    //@FXML BarChart barChart;
     @FXML private BarChart<?, ?> barChart;
-    @FXML private CategoryAxis xAxis;
-    @FXML private NumberAxis yAxis;
 
     public void handleButtonHome(ActionEvent event) throws IOException {
         Parent menuScreen = FXMLLoader.load(getClass().getResource("/Presentation/welcome.fxml"));
@@ -47,32 +38,22 @@ public class ControllerCleaningCentralStatistics {
     }
 
     /**
-     * The method will display a barChart of the incoming number of orders and items, which were handed-in at the delivery points.
-     * TODO get the right values
+     * The method will display a barChart of the incoming number of orders, sorted after the different routes their
+     * corresponding delivery point is on.
      */
     public void handleShowIncomingOrders() {
-        XYChart.Series set1 = new XYChart.Series<>();
-        set1.setName("NORTH");
+        XYChart.Series dataSet1 = new XYChart.Series<>();
+        dataSet1.setName("EAST");
+        dataSet1.getData().add(Adapter.DBInstance().getIncomingOrders().get(0));
 
-        set1.getData().add(new XYChart.Data("orders", 20));
-        set1.getData().add(new XYChart.Data("items", 40));
-        barChart.getData().addAll(set1);
+        XYChart.Series dataSet2 = new XYChart.Series<>();
+        dataSet2.setName("NORTH");
+        dataSet2.getData().add(Adapter.DBInstance().getIncomingOrders().get(1));
 
-        XYChart.Series set2 = new XYChart.Series<>();
-        set2.setName("SOUTH");
-        set2.getData().add(new XYChart.Data("orders", 10));
-        set2.getData().add(new XYChart.Data("items", 30));
-        barChart.getData().add(set2);
+        XYChart.Series dataSet3 = new XYChart.Series<>();
+        dataSet3.setName("SOUTH");
+        dataSet3.getData().add(Adapter.DBInstance().getIncomingOrders().get(2));
 
-        XYChart.Series set3 = new XYChart.Series<>();
-        set3.setName("EAST");
-        set3.getData().add(new XYChart.Data("orders", 50));
-        set3.getData().add(new XYChart.Data("items", 100));
-        barChart.getData().add(set3);
-
-        // "get orders from route" get Orders From DeliveryPoint where route = North
-        // count these orders
-        // get items.size() from each order and calculate
-
+        barChart.getData().addAll(dataSet1, dataSet2, dataSet3);
     }
 }
